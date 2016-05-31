86c71c982bf66a37de79533dc27b7d50ca7e8aa8
Chanwoong Kim <devk0817@gmail.com>
2016-05-31 14:00:33 +0900

    어른의 사정

--- SmartFormat.NET-Korean.Tests/Extensions/KoreanFormatterTests.cs
+++ SmartFormat.NET-Korean.Tests/Extensions/KoreanFormatterTests.cs
@@ -18,17 +18,17 @@ namespace KoreanParticleFormatter.Tests.Extensions
 			}
 		}
 
-		[TestCase("{0:ko:아} 안녕", "철수", "철수야 안녕")]
+		[TestCase("{0:ko:아} 안녕", "나오", "나오야 안녕")]
 		[TestCase("{0:ko:을} 칼로 깎는다", "사과", "사과를 칼로 깎는다")]
-		[TestCase("{0:ko:으로}", "김병장", "김병장으로")]
-		[TestCase("{0:ko:으로}", "송하나=디바", "송하나=디바로")]
+		[TestCase("{0:ko:으로}", "모리안", "모리안으로")]
+		[TestCase("{0:ko:으로}", "퍼거스=대장장이", "퍼거스=대장장이로")]
 		public void Test_simple(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
 		}
 
-		[TestCase("{0:으로}", "송하나=디바", "송하나=디바로")]
-		[TestCase("{0:으로}", "송하나(디바(D.Va))", "송하나(디바(D.Va))로")]
+		[TestCase("{0:으로}", "퍼거스=대장장이", "퍼거스=대장장이로")]
+		[TestCase("{0:으로}", "퍼거스(Ferghus)", "퍼거스(Ferghus)로")]
 		public void Test_Implicit(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
@@ -49,16 +49,16 @@ namespace KoreanParticleFormatter.Tests.Extensions
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
 		}
 
-		[TestCase("{0:ko:으로}", "포켓몬(피카츄)", "포켓몬(피카츄)으로")]
-		[TestCase("{0:ko:으로}", "송하나(디바(D.Va))", "송하나(디바(D.Va))로")]
-		[TestCase("{0:가}?", "임창정,,,", "임창정,,,이?")]
+		[TestCase("{0:ko:으로}", "모리안(여신)", "모리안(여신)으로")]
+		[TestCase("{0:ko:으로}", "퍼거스(대장장이(Ferghus))", "퍼거스(대장장이(Ferghus))로")]
+		[TestCase("{0:가}?", "모리안,,,", "모리안,,,이?")]
 		[TestCase("{0:을} 샀다.", "<듀랑고>", "<듀랑고>를 샀다.")]
 		public void Test_Filter(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
 		}
 
-		[TestCase("{0:ko:은} {1:ko:을} 썼다", "피카츄", "전광석화", "피카츄는 전광석화를 썼다")]
+		[TestCase("{0:ko:은} {1:ko:로} 불린다.", "나오", "검은사신", "나오는 검은사신으로 불린다.")]
 		public void Test_Arg2(string format, object arg0, object arg1, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0, arg1));
@@ -71,15 +71,15 @@ namespace KoreanParticleFormatter.Tests.Extensions
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
 		}
 
-		[TestCase("{0:ko(아):{}} 안녕", "철수", "철수야 안녕")]
-		[TestCase("{0:ko(아):아}", "철수", "아야")]
+		[TestCase("{0:ko(아):{}} 안녕", "나오", "나오야 안녕")]
+		[TestCase("{0:ko(아):아}", "나오", "아야")]
 		public void Test_FormattingOption(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
 		}
 
-		[TestCase("{0:ko(은):{}} {1:ko(으로):{}} 진화 했다!", "피카츄", "라이츄", "피카츄는 라이츄로 진화 했다!")]
-		[TestCase("{0:은} {1:으로} 진화 했다!", "피카츄", "라이츄", "피카츄는 라이츄로 진화 했다!")]
+		[TestCase("{0:ko(은):{}} {1:ko(으로):{}} 불린다.", "나오", "검은사신", "나오는 검은사신으로 불린다.")]
+		[TestCase("{0:은} {1:으로} 변신 했다!", "밀레시안", "팔라딘", "밀레시안은 팔라딘으로 변신 했다!")]
 		public void Test_FormattingOption_Arg2(string format, object arg0, object arg1, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0, arg1));
@@ -97,36 +97,36 @@ namespace KoreanParticleFormatter.Tests.Extensions
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
 		}
 
-		[TestCase("{0:이다}", "피카츄", "피카츄다")]
-		[TestCase("{0:이다}", "버터플", "버터플이다")]
-		[TestCase("{0:이에요}", "피카츄", "피카츄예요")]
-		[TestCase("{0:이에요}", "버터플", "버터플이에요")]
-		[TestCase("{0:입니다}", "피카츄", "피카츄입니다")]
-		[TestCase("{0:입니다}", "버터플", "버터플입니다")]
-		[TestCase("{0:이다}", "God", "God(이)다")]
-		[TestCase("{0:이에요}", "God", "God(이)에요")]
-		[TestCase("{0:입니다}", "God", "God입니다")]
-		[TestCase("{0:였습니다}", "God", "God(이)었습니다")]
-		[TestCase("{0:였습니다}", "버터플", "버터플이었습니다")]
-		[TestCase("{0:였습니다}", "피카츄", "피카츄였습니다")]
-		[TestCase("{0:이었다}", "피카츄", "피카츄였다")]
-		[TestCase("{0:이었지만}", "피카츄", "피카츄였지만")]
-		[TestCase("{0:이지만}", "피카츄", "피카츄지만")]
-		[TestCase("{0:이지만}", "버터플", "버터플이지만")]
-		[TestCase("{0:지만}", "피카츄", "피카츄지만")]
-		[TestCase("{0:지만}", "버터플", "버터플이지만")]
-		[TestCase("{0:다}", "피카츄", "피카츄다")]
-		[TestCase("{0:다}", "버터플", "버터플이다")]
-		[TestCase("{0:이에요}", "피카츄", "피카츄예요")]
-		[TestCase("{0:이에요}", "버터플", "버터플이에요")]
-		[TestCase("{0:고}", "피카츄", "피카츄고")]
-		[TestCase("{0:고}", "버터플", "버터플이고")]
-		[TestCase("{0:고}", "리자몽", "리자몽이고")]
-		[TestCase("{0:여서}", "피카츄", "피카츄여서")]
-		[TestCase("{0:여서}", "버터플", "버터플이어서")]
-		[TestCase("{0:이어서}", "피카츄", "피카츄여서")]
-		[TestCase("{0:이어서}", "버터플", "버터플이어서")]
-		[TestCase("{0:라고라}?", "버터플", "버터플이라고라?")]
+		[TestCase("{0:이다}", "나오", "나오다")]
+		[TestCase("{0:이다}", "키홀", "키홀이다")]
+		[TestCase("{0:이에요}", "나오", "나오예요")]
+		[TestCase("{0:이에요}", "키홀", "키홀이에요")]
+		[TestCase("{0:입니다}", "나오", "나오입니다")]
+		[TestCase("{0:입니다}", "키홀", "키홀입니다")]
+		[TestCase("{0:이다}", "Nao", "Nao(이)다")]
+		[TestCase("{0:이에요}", "Nao", "Nao(이)에요")]
+		[TestCase("{0:입니다}", "Nao", "Nao입니다")]
+		[TestCase("{0:였습니다}", "Nao", "Nao(이)었습니다")]
+		[TestCase("{0:였습니다}", "키홀", "키홀이었습니다")]
+		[TestCase("{0:였습니다}", "나오", "나오였습니다")]
+		[TestCase("{0:이었다}", "나오", "나오였다")]
+		[TestCase("{0:이었지만}", "나오", "나오였지만")]
+		[TestCase("{0:이지만}", "나오", "나오지만")]
+		[TestCase("{0:이지만}", "키홀", "키홀이지만")]
+		[TestCase("{0:지만}", "나오", "나오지만")]
+		[TestCase("{0:지만}", "키홀", "키홀이지만")]
+		[TestCase("{0:다}", "나오", "나오다")]
+		[TestCase("{0:다}", "키홀", "키홀이다")]
+		[TestCase("{0:이에요}", "나오", "나오예요")]
+		[TestCase("{0:이에요}", "키홀", "키홀이에요")]
+		[TestCase("{0:고}", "나오", "나오고")]
+		[TestCase("{0:고}", "키홀", "키홀이고")]
+		[TestCase("{0:고}", "모리안", "모리안이고")]
+		[TestCase("{0:여서}", "나오", "나오여서")]
+		[TestCase("{0:여서}", "키홀", "키홀이어서")]
+		[TestCase("{0:이어서}", "나오", "나오여서")]
+		[TestCase("{0:이어서}", "키홀", "키홀이어서")]
+		[TestCase("{0:라고라}?", "키홀", "키홀이라고라?")]
 		public void Test_Ida(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
@@ -135,31 +135,31 @@ namespace KoreanParticleFormatter.Tests.Extensions
 		[TestCase("{0:ko(으로):으로}", "용사", "으로로")]
 		[TestCase("{0:ko(으로):으로}", "마법", "으로으로")]
 		[TestCase("{0:ko(으로):{}}", "마법", "마법으로")]
-		[TestCase("{0:ko(으로):{}}", "피카츄(Lv.25)", "피카츄(Lv.25)로")]
-		[TestCase("{0:ko(으로):{}}", "피카(?)츄", "피카(?)츄로")]
+		[TestCase("{0:ko(으로):{}}", "나오(Lv.25)", "나오(Lv.25)로")]
+		[TestCase("{0:ko(으로):{}}", "퍼거(?)스", "퍼거(?)스로")]
 		[TestCase("{0:ko(으로):{}}", "헬로월드!", "헬로월드!로")]
 		[TestCase("{0:ko(으로):{}}", "?_?", "?_?(으)로")]
 		[TestCase("{0:ko(으로):<i>{}</i>}", "마법", "<i>마법</i>으로")]
-		[TestCase("{0:ko(로서):{}}", "피카츄", "피카츄로서")]
-		[TestCase("{0:ko(로서):{}}", "버터플", "버터플로서")]
-		[TestCase("{0:ko(로서):{}}", "고라파덕", "고라파덕으로서")]
-		[TestCase("{0:ko(로부터):{}}", "피카츄", "피카츄로부터")]
-		[TestCase("{0:ko(로부터):{}}", "버터플", "버터플로부터")]
-		[TestCase("{0:ko(로부터):{}}", "고라파덕", "고라파덕으로부터")]
+		[TestCase("{0:ko(로서):{}}", "나오", "나오로서")]
+		[TestCase("{0:ko(로서):{}}", "키홀", "키홀로서")]
+		[TestCase("{0:ko(로서):{}}", "모리안", "모리안으로서")]
+		[TestCase("{0:ko(로부터):{}}", "나오", "나오로부터")]
+		[TestCase("{0:ko(로부터):{}}", "키홀", "키홀로부터")]
+		[TestCase("{0:ko(로부터):{}}", "모리안", "모리안으로부터")]
 		public void Test_Euro(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
 		}
 
 
-		[TestCase("{0:도}", "피카츄", "피카츄도")]
-		[TestCase("{0:도}", "고라파덕", "고라파덕도")]
+		[TestCase("{0:도}", "나오", "나오도")]
+		[TestCase("{0:도}", "모리안", "모리안도")]
 		[TestCase("{0:에서}", "판교", "판교에서")]
 		[TestCase("{0:에서는}", "판교", "판교에서는")]
-		[TestCase("{0:께서도}", "각하", "각하께서도")]
-		[TestCase("{0:의}", "이상해씨", "이상해씨의")]
-		[TestCase("{0:만}", "리자몽", "리자몽만")]
-		[TestCase("{0:하고}", "버터플", "버터플하고")]
+		[TestCase("{0:께서도}", "선생님", "선생님께서도")]
+		[TestCase("{0:의}", "나오", "나오의")]
+		[TestCase("{0:만}", "모리안", "모리안만")]
+		[TestCase("{0:하고}", "키홀", "키홀하고")]
 		public void Test_InvariantParticles(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
